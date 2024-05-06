49f0a49cf9d0cc24eb8ae7933e082dfc7be1f9c4
Carlos Chávez <u201910317@upc.edu.pe>
2024-05-05 20:03:23 -0500

    feat: nav-bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIcon,
+    MatMenu,
+    MatToolbar,
+    MatMenuTrigger
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
