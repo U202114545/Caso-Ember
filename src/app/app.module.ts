1bbc6317ff329116f365faf8d740aa9b440e2ddf
U202114545 <u202114545@upc.edu.pe>
2024-05-05 19:28:50 -0600

    Segundo avance de plantilla esqueleto del caso Ember

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BundlesComponent } from './store/components/bundles/bundles.component';
+import { HomeComponent } from './public/home/home.component';
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BundlesComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
